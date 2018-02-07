b3aa3f3797c8b672c4ecf94bda6c8c66508d4b4f
Jerry Wong <jerry.wong@iproperty.com>
2018-02-07 17:46:28 +0800

    update culture

--- Traffiti_Api/Controllers/CommonController.cs
+++ Traffiti_Api/Controllers/CommonController.cs
@@ -17,17 +17,33 @@ namespace Traffiti_Api.Controllers
 
         public string CalculateDateTime(DateTime dt, int lang_id)
         {
-            
+            string language = "en-us";
+            switch (lang_id)
+            {
+                case 1:
+                    language = "en-us";
+                    break;
+                case 2:
+                    language = "zh-hk";
+                    break;
+                case 3:
+                    language = "zh-cn";
+                    break;
+                default: language = "zh-hk";
+                    break;
+            }
+            CultureInfo ci = new CultureInfo(language);
+
             string text = string.Empty;
             TimeSpan ts = new TimeSpan();
             ts = DateTime.Now - dt;
             if (ts.TotalMinutes < 10)
             {
-                text = (string)HttpContext.GetGlobalResourceObject("Common", "recently");
+                text = (string)HttpContext.GetGlobalResourceObject("Common", "recently", ci);
             }
             else if (ts.TotalHours < 2)
             {
-                text = (string)HttpContext.GetGlobalResourceObject("Common", "hour");
+                text = (string)HttpContext.GetGlobalResourceObject("Common", "hour", ci);
             }
             else
             {
